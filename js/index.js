947d29c677afd9a0582a3b7bfa172e704088cea7
StealthWorm <thierrypitela@hotmail.com>
2024-09-26 21:36:51 -0300

    feat: finished card generator vanilla JS

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const routes = {
+  '/': {
+    linkLabel: 'Home',
+    content: `I am in home page`,
+  },
+  '/result': {
+    linkLabel: 'Result',
+    content: `I am in about page`,
+  },
+};
---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+function renderResult(e, t, n) {
+  let a = document.getElementById('container');
+  a.innerHTML = `
+  <div id="result-container">
+    <img
+      src="assets/landing-page.png"
+      alt="man-painting-wall"
+      id="hero-banner" />
+
+    <div id="card-container">
+      <button id="reset-form-button" onclick="handleResetForm()"><  Gerar outro cart\xe3o</button>
+
+      <div id="card">
+        <img 
+          src="assets/logo-symbol.svg" 
+          alt="logo-icon" alt="Home - Resultados Digitais - Homem Criando Cart\xe3o" 
+          loading="lazy"
+        />
+
+        <div class="vl"></div>
+
+        <div id="card-info">
+          <p>${e}</p>
+          <p>${n}</p>
+          <p>${t}</p>
+        </div>
+      </div>
+
+      <button 
+        class="button-primary" 
+        disabled
+      >
+        ⬇  BAIXAR CART\xc3O 
+      </button>
+      <a href="https://app.rdstation.com.br/signup" target="_blank">
+        Fazer um teste gr\xe1tis do RD Station Marketing →
+      </a>
+    </div>
+  </div>
+  `;
+}
+function handleResetForm() {
+  location.reload();
+}
+function handleSubmit(e) {
+  e.preventDefault();
+  let t = document.getElementById('nome').value,
+    n = document.getElementById('email').value,
+    a = document.getElementById('telefone').value;
+  renderResult(t, n, a);
+}
+(document.getElementById('year').innerHTML = new Date().getFullYear()),
+  document.getElementById('telefone').addEventListener('input', function (e) {
+    let t = e.target.value.replace(/\D/g, '');
+    (t = (t = t.replace(/^(\d{2})(\d)/g, '($1) $2')).replace(
+      /(\d{4})(\d{1,4})$/,
+      '$1-$2'
+    )),
+      (e.target.value = t);
+  });
